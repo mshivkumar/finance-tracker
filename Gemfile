5509d8a2d2dedbc839c1f67776b79bf9b9e8c57d
mshivkumar <mshiv317@gmail.com>
2020-05-09 21:07:44 +0530

    add friend association

--- Gemfile
+++ Gemfile
@@ -25,9 +25,8 @@ gem 'devise', '~> 4.2'
 gem 'devise-bootstrap-views', '~> 1.0'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
-gem 'solargraph', group: :development
-gem 'rubocop', group: :development
 gem 'iex-ruby-client'
+gem 'font-awesome-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,27 @@
 class Stock < ApplicationRecord
 
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker_symbol, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
         publishable_token: Rails.application.credentials.iex_client[:sandbox_api_publishable_key],
         secret_token: Rails.application.credentials.iex_client[:sandbox_api_secret_key],
         endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.price(ticker_symbol)
+    # client.price(ticker_symbol)
+    begin
+      new(name: client.company(ticker_symbol).company_name, ticker_symbol: ticker_symbol, price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
+
+  def self.check_db(ticker_symbol)
+    where(ticker_symbol: ticker_symbol).first
+  end
+
 
 end
